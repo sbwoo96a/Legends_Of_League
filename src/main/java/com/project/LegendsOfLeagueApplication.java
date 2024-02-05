96b26a4b1f98f734171eab0f19af12019a56a391
hyunwoo <hyunwoo0318@naver.com>
2024-02-05 16:44:23 +0900

    feat/initial project

---
+++ src/main/java/com/project/LegendsOfLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendsOfLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendsOfLeagueApplication.class, args);
+	}
+
+}
